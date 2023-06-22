db64bb00681a775749534d34bb4e88c397ef027d
Almira Mahsa <almiramahsa9@gmail.com>
2023-06-22 16:25:58 +0700

    set SEO, Meta, etc

--- frontend/app.vue
+++ frontend/app.vue
@@ -1,5 +1,7 @@
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+    <div>
+        <h1 class="text-xl font-light text-emerald-500">
+            Holla testing pertama kali pake NUXT Js dan Tailwind CSS
+        </h1>
+    </div>
 </template>
--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,4 +1,39 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+    modules: ["@nuxtjs/tailwindcss"],
+    css: ["@/assets/css/main.css"],
+    app: {
+        head: {
+            title: "Nature Social App",
+            meta: [
+                {
+                    charset: "utf-u",
+                },
+                {
+                    name: "viewport",
+                    content: "width=device-width, initial-scale=1",
+                },
+                {
+                    hid: "description",
+                    name: "description",
+                    content: "Nature Social App",
+                },
+                {
+                    name: "format-detection",
+                    content: "telephone-no",
+                },
+            ],
+            link: [
+                {
+                    rel: "stylesheet",
+                    href: "https://fonts.googleapis.com/css2?family=Playfair:wght@300;400;500;600;800&display=swap",
+                },
+                {
+                    rel: "icon",
+                    type: "image/x-icon",
+                    href: "favicon.ico",
+                },
+            ],
+        },
+    },
+});
